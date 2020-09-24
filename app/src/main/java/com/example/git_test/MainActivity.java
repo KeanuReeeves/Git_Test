8eda42d127705cf34c4759c9566eb4db93752bf9
Keanu Reeves <tzas95@gmail.com>
2020-09-24 16:13:41 +0200

    Elso Commit és push

--- app/src/main/java/com/example/git_test/MainActivity.java
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.git_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    RelativeLayout Pontok;
+    TextView textPontok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Pontok=findViewById(R.id.Ne_legyel_hulye);
+        textPontok=findViewById(R.id.textPontok);
+        Pontok.setOnClickListener();
     }
 }
